451d8c431da02fe09d7985bd4ab85ad31a2ef66a
U_JINYEOL <100745412+U-Jinyeol@users.noreply.github.com>
2022-07-13 23:34:21 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 30 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,22 @@
+function solution(price, money, count) {
+    var answer = 0;
+
+    for (let i = 1; i <= count; i++) {
+        answer += price * i
+        } if (answer > money) {
+            answer = answer - money
+        } else {
+            answer = 0
+        }
+    return answer;
+}
+
+// function solution(price, money, count) {
+//     let answer = 0;
+
+//     for (let i = 1; i <= count; i++) {
+//         answer += price * i;
+//     }
+
+//     return answer > money ? answer - money : 0;
+// }
